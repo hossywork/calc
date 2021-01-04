4f629e930655c8c792c5c92215d2872e70c6334f
hoshino <other69de1919@gmail.com>
2021-01-04 15:51:49 +0900

    Initial commit

---
+++ src/task/Dentaku/Main.java
@@ -0,0 +1,24 @@
+package task.Dentaku;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+	// 標準入力を受け取り、式を表示
+        Scanner sc = new Scanner(System.in);
+        System.out.println("式を入力してください");
+        System.out.print("式：");
+        String formula = sc.nextLine();
+        System.out.println(formula);
+    // 演算子を抜き取り、数値を変換
+        String[] numerals = formula.split("[¥+/*-]");
+        double[] numbers = new double[numerals.length];
+        for (int i = 0; i < numbers.length; i++){
+            numbers[i] = Double.parseDouble(numerals[i]);
+        }
+        System.out.println(numbers[0]);
+    //　演算子をそれぞれ計算に組み込んでいく
+
+
+
+    }
+}
