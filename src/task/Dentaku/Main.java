e685b76f62cd159894383a319ffd07af65eba07e
hoshino <other69de1919@gmail.com>
2021-01-12 16:46:55 +0900

    add 乗算、除算に使う要素を優先要素として取り出しました。

--- src/task/Dentaku/Main.java
+++ src/task/Dentaku/Main.java
@@ -1,25 +1,31 @@
 package task.Dentaku;
+import java.util.Arrays;
 import java.util.Scanner;
 public class Main {
-
     public static void main(String[] args) {
-	// 標準入力を受け取り、式を表示
+        // 0 標準入力を受け取り、式を表示
         Scanner sc = new Scanner(System.in);
         System.out.println("式を入力してください");
         System.out.print("式：");
         String formula = sc.nextLine();
         System.out.println(formula);
-    // 演算子を抜き取り、数値を変換
-        String[] numerals = formula.split("[¥+/*-]");
-        double[] numbers = new double[numerals.length];
-        for (int i = 0; i < numbers.length; i++){
-            numbers[i] = Double.parseDouble(numerals[i]);
-        }
-        System.out.println(numbers[0]);
-    //　演算子をそれぞれ計算に組み込んでいく
-        String[] operators = formula.split("[0 - 9]+");
-        System.out.println(operators[1]);
+        // 1 ＋、ーの演算子を除外する、優先する要素を抜き取る（subFormula)
+        String[] semiFormula = formula.split("[¥+-]");
+        // 2 ＋、－の演算子の配列を作る(addSub)
+        String[] addSub = formula.split("[0-9¥/¥*]+");
+        System.out.println(Arrays.deepToString(addSub));
+        // 3 優先要素を繰り返し計算する　// a for文で条件式をつくる
+        Double[] semiResult = new Double[semiFormula.length];
+        for (int b = 0; b < semiFormula.length; b++){
+        // b 優先要素の中から数値だけを取り出し、double型に変換
+            String[] numerals = semiFormula[b].split("[*¥/]");
+            Double[] numbers = new Double[numerals.length];
 
+
+        }
+        // 4 優先処理の結果から＋、－の繰り返し計算する
+        // 5
+        // 6
 
     }
 }
